633d71fcd753e08bd734093dafeeea2a5c7cf1cc
JohannesWiesner <jw73fexo@studserv.uni-leipzig.de>
2020-06-10 11:30:54 +0200

    major changes to report module

--- skplot/report.py
+++ skplot/report.py
@@ -1,34 +1,22 @@
 import pandas as pd
-import re
 from skplot import preparation
 
-# FIXME: See extraction module. The type of measure should already be
-# added in extraction.extract_single_plot data. With this, you don't have
-# to use regex anymore.
 def get_categorical_stats_report(plot_data,**kwargs):
     
     plot_df = preparation.get_plot_df(plot_data,**kwargs)
-
-    # get list of names of all hyperparameters (search for categories that contain '__')
-    measure_names = plot_df['measure_name'].cat.categories.to_list()
-    categorical_params = [measure_name for measure_name in measure_names if re.match('^.+__.+$',measure_name)]
+    categorical_stats_df = preparation.get_categorical_stats_df(plot_df)
     
-    # subset to data frame that only contains hyperparameters
-    plot_df_ = plot_df.loc[plot_df['measure_name'].isin(categorical_params),:]
-    plot_df_.loc[:,'measure_name'] = pd.Categorical(plot_df_['measure_name'],categories=categorical_params,ordered=True)
-        
-    # treat hyperparameter values as categories
-    plot_df_.loc[:,'measure_value'] = plot_df_['measure_value'].astype('category')
+    index_vars = 'model_number'
     
-    # count hyperparameter categories
-    categorical_stats_report = plot_df_.groupby(['model_number','measure_name','measure_value',]).count().reset_index()
-    categorical_stats_report.rename(columns={'outer_fold':'count'},inplace=True)
-    categorical_stats_report.loc[:,'count'].fillna(0,inplace=True)
+    if 'model_name' in plot_df.colums:
+        index_vars = ['model_number','model_name']
+    else:
+        index_vars = 'model_number'
     
-    categorical_stats_report = pd.pivot_table(data=categorical_stats_report,
+    categorical_stats_report = pd.pivot_table(data=categorical_stats_df,
                                               columns=['measure_name','measure_value'],
                                               values='count',
-                                              index='model_number')
+                                              index=index_vars)
     
     return categorical_stats_report
 
