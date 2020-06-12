753dbc565a03168e0541fc22956d8bf1e2b15bd3
JohannesWiesner <jw73fexo@studserv.uni-leipzig.de>
2020-06-12 15:10:45 +0200

    worked on lineplot_scores function

--- skplot/plotting.py
+++ skplot/plotting.py
@@ -15,6 +15,16 @@ import numpy as np
 
 # TO-DO: Make plot 'pretty', e.g. set figure background transparent with plt.gcf().patch.set_alpha(0.0)
 # TO-DO: define one function for setting style (setting seaborn stile, font, transparent plot, etc.)
+
+def get_number_of_models(plot_df):
+    return plot_df['model_number'].value_counts().count()
+
+def get_number_of_measures(plot_df):
+    return plot_df['measure_name'].value_counts().count()
+
+def get_measure_labels(plot_df):
+    return plot_df['measure_label'].unique()
+
 def save_plot(dst_dir,filename):
     if dst_dir:
         if not filename:
@@ -39,7 +49,8 @@ def set_x_and_y_axis_layout(g,xlim='auto',ylim='auto',
         loc_x = plticker.MultipleLocator(base=x_tick_spacing)
         plt.gca().xaxis.set_major_locator(loc_x)
 
-def plot_mean_lines(axes_flatten,stats_df,n_models,n_measures):
+def plot_mean_lines(axes_flatten,continuous_stats_df,n_models,n_measures):
+    
     # get colors for each score
     current_palette = sns.color_palette()
     colors = []
@@ -51,7 +62,7 @@ def plot_mean_lines(axes_flatten,stats_df,n_models,n_measures):
     
     for idx in range(0,n_models):    
         for line_idx in range(0,n_measures):
-            axes_flatten[idx].axhline(y=stats_df.loc[row_idx,'mean'],color=colors[line_idx])
+            axes_flatten[idx].axhline(y=continuous_stats_df.loc[row_idx,'mean'],color=colors[line_idx])
             row_idx += 1
             
 def get_nested_titles_and_labels(iterator_dict):
@@ -108,43 +119,37 @@ def draw_nested_xaxes(g,y_axis_title,iterator_dict):
         # set title
         ax.set_xlabel(secondary_xaxis_elements.columns[idx])
 
-def lineplot_scores(plot_data,measures=None,iterator_dict=None,measure_labels=None,
-                    x_tick_spacing='auto',y_tick_spacing='auto',
-                    x_axis_title='Outer Fold',y_axis_title ='Score',
-                    xlim='auto',ylim='auto',col_wrap=1,legend_title='Measures',
-                    mean_lines=True,dst_dir=None,filename=None,height=4,aspect=2):
+def lineplot_scores(plot_df,height=4,aspect=2,col_wrap=1,x_tick_spacing='auto',
+                    y_tick_spacing='auto',x_axis_title='Outer Fold',y_axis_title ='Score',xlim='auto',ylim='auto',
+                    legend_title='Measures',mean_lines=True,dst_dir=None,filename=None):
+    
+    # subset to only measures with interval scale
+    plot_df = plot_df.loc[plot_df['scale_type'] == 'interval']
     
     # seaborn settings
     sns.set(font='Open Sans',style='whitegrid')
-
-    ## validate input #########################################################
-    preparation.validate_measures(measures,measure_labels)
-    
-    # prepare plot ############################################################
-    plot_df = preparation.get_plot_df(plot_data=plot_data,
-                                                   measures=measures,
-                                                   iterator_dict=iterator_dict,
-                                                   measure_labels=measure_labels)
     
     # get variables that are required for later functions #####################
-    n_models = preparation.get_number_of_models(plot_df)
-    n_measures = preparation.get_number_of_measures(plot_df)
-    stats_df = preparation.get_stats_df(plot_df)
+    n_models = get_number_of_models(plot_df)
+    n_measures = get_number_of_measures(plot_df)
+    continuous_stats_df = preparation.get_continuous_stats_df(plot_df)
     
-    if iterator_dict:
-        iterator_keys,iterator_combinations = preparation.get_keys_and_combos(iterator_dict)
-        model_titles = preparation.get_model_titles(iterator_keys,iterator_combinations)
+    # Plot measures as lines ##################################################
+    g = sns.FacetGrid(plot_df,
+                      col='model_number',
+                      height=height,
+                      aspect=aspect,
+                      col_wrap=col_wrap)
     
-    # Plot measures as lines ####################################################
-    g = sns.FacetGrid(plot_df,col='model_number',height=height,aspect=aspect,col_wrap=col_wrap)
     g.map(sns.lineplot,'outer_fold','measure_value','measure_name')
-
+    
     # set matplotlib variables that are required for later functions
     axes_flatten = g.axes.flatten()
     axes_iterator = g.axes.flat
     
     # draw subplot title(s) ###################################################
-    if iterator_dict:
+    if 'model_name' in plot_df.columns:
+        model_titles = plot_df['model_name'].unique()
         for title,ax in zip(model_titles,axes_iterator):
             ax.set_title(title)
     
@@ -152,41 +157,36 @@ def lineplot_scores(plot_data,measures=None,iterator_dict=None,measure_labels=No
         g.set_titles('') 
                 
     ## Add and modify legend title and labels #################################
-    # FIXME: If only one measure is provided, legend is not 'outside' plot
-    # FIMXE: setting legend title and labels is happening automatically
-    # when providing measure_labels. Check why this error happens.
+    # FIXME: Legend is sometimes inside plot
     g.add_legend()
     legend_texts = g._legend.texts
     legend_texts[0].set_text(legend_title)
-
-    if measure_labels:
-        if isinstance(measure_labels,list):
-            for t, l in zip(legend_texts[1:],measure_labels): 
-                t.set_text(l)
+    
+    if 'measure_label' in plot_df.columns:
         
-        elif isinstance(measure_labels,str):
-            legend_texts[1].set_text(measure_labels)
-
+        measure_labels = get_measure_labels(plot_df)
+     
+        for t,l in zip(legend_texts[1:],measure_labels):
+            t.set_text(l)
+    
     ## Modify x and y-axis intervals ##########################################
     set_x_and_y_axis_layout(g,xlim,ylim,x_tick_spacing,y_tick_spacing)
-
+    
     # Modify x and y-axis titles ##############################################
-    # FIXME: Why is it not possible to use g.axes.flatten() iterator for setting
-    # axis titles?
-    for idx in range(0,n_models):
-        axes_flatten[idx].set_ylabel(y_axis_title)
-        axes_flatten[idx].set_xlabel(x_axis_title)
+    for ax in axes_flatten:
+        ax.set_ylabel(y_axis_title)
+        ax.set_xlabel(x_axis_title)
     
     ## Plot Mean Lines ########################################################
     if mean_lines == True:
         plot_mean_lines(axes_flatten=axes_flatten,
-                        stats_df=stats_df,
+                        continuous_stats_df=continuous_stats_df,
                         n_models=n_models,
                         n_measures=n_measures)
-
+    
     ## Save plot ##############################################################
     save_plot(dst_dir,filename)
-    
+
 # FIXME: if only one model is provided and separate measures=True, boxplots
 # are not centered
 # TO-DO: When using kind = 'box', draw mean line
