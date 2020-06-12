442ccf7deae6260dd33b402380140c575e7a849a
JohannesWiesner <jw73fexo@studserv.uni-leipzig.de>
2020-06-12 15:11:15 +0200

    worked on get_continuous_stats_report

--- skplot/report.py
+++ skplot/report.py
@@ -1,14 +1,19 @@
 import pandas as pd
 from skplot import preparation
 
-def get_categorical_stats_report(plot_data,**kwargs):
+def check_plot_df(plot_df):
     
-    plot_df = preparation.get_plot_df(plot_data,**kwargs)
+    if 'scale_type' not in plot_df.columns:
+        raise ValueError('input plot_df must contain a column scale_type')
+
+def get_categorical_stats_report(plot_df):
+    
+    check_plot_df(plot_df)
     categorical_stats_df = preparation.get_categorical_stats_df(plot_df)
     
     index_vars = 'model_number'
     
-    if 'model_name' in plot_df.colums:
+    if 'model_name' in plot_df.columns:
         index_vars = ['model_number','model_name']
     else:
         index_vars = 'model_number'
@@ -20,14 +25,10 @@ def get_categorical_stats_report(plot_data,**kwargs):
     
     return categorical_stats_report
 
-# TO-DO: allow for measure_names
-# TO-DO: allow to reorder the columns to make it more pretty, i.e.
-# the order how measures is defined should define the order of how colums appear
-def get_continuous_stats_report(plot_data,**kwargs):
+def get_continuous_stats_report(plot_df):
     
-    plot_df = preparation.get_plot_df(plot_data,**kwargs)
+    check_plot_df(plot_df)
     continuous_stats_df = preparation.get_continuous_stats_df(plot_df)
-    
     continuous_stats_df = continuous_stats_df.round({'mean':2,'median':2,'std': 2})
     continuous_stats_df['descriptive_statistics'] = continuous_stats_df.agg('{0[mean]} ({0[std]})'.format,axis=1)
     
@@ -41,15 +42,6 @@ def get_continuous_stats_report(plot_data,**kwargs):
                                              values='descriptive_statistics',
                                              index=index_vars,
                                              aggfunc='first')
-    
-    # convert categorical columns to objects in order allow multiple 
-    # stats reports to be concatenated together at later stages. Without this, you get error
-    # See: https://github.com/pandas-dev/pandas/issues/19136#issuecomment-597884616
-    continuous_stats_report.columns = continuous_stats_report.columns.astype(list)
-    
-    # FIXME: If row MultiIndex (that is (model_number,model_name), create single
-    # index from this, and also convert to list in order to fix problem as above
-    # continuous_stats_report.index = continuous_stats_report.index.astype(list)
     
     return continuous_stats_report
 
