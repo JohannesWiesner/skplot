8983dcfa7645155f3ab74a0c9c30c06fb205df40
Wiesner <joh.wiesner@gmail.com>
2020-10-21 13:21:47 +0200

    worked on example script. added more documentation and also show lineplot function.

--- examples/breast_cancer.py
+++ examples/breast_cancer.py
@@ -17,20 +17,23 @@ from sklearn.model_selection import cross_validate
 
 ## Set user defined variables #################################################
 
-RANDOM_SEED=42
-LR_C=np.linspace(start=1,stop=10,num=5,endpoint=True)
-INNER_CV=5
-OUTER_CV=5
+RANDOM_SEED = 42
+LR_C = np.linspace(start=1,stop=10,num=5,endpoint=True)
+INNER_CV = 5
+OUTER_CV = 10
 
 ## Load Data  #################################################################
 
 X,y = datasets.load_breast_cancer(return_X_y=True)
 
-## Run classification pipeline for different solvers and penalty types ########
+## Run several classification pipelines with different combinations of input
+# parameters 
 
-# The output of each combination of solver and penalty type will be stored in a list
+# The output of each pipeline must be stored in a list
 scores_list = []
 
+# Try out for different amounts of explained variances and different regularization
+# types
 for n_components in [0.9,0.1]:
     for penalty_type in ['l2','l1']:
     
@@ -60,7 +63,7 @@ for n_components in [0.9,0.1]:
         
         # define inner and outer folds
         skf_inner = StratifiedKFold(n_splits=INNER_CV)
-        skf_outer = StratifiedKFold(n_splits=INNER_CV)
+        skf_outer = StratifiedKFold(n_splits=OUTER_CV)
         
         # implement GridSearch (inner cross validation)
         grid = GridSearchCV(lr_pipe,
@@ -81,31 +84,44 @@ for n_components in [0.9,0.1]:
     
 # Extract data ################################################################
 
-# get the coefficient value for each pca component 
+# Create your personal custom function: get the coefficient value for each feature
 def get_intercept_value(scores,idx):
     return scores['estimator'][idx].best_estimator_._final_estimator.intercept_[0]
 
+# Extract information for all different pipelines using build-in settings
+# and your custom function
 scores_df = extraction.get_measures_df(scores_list,
                                        data_kind=['lr__C','train_and_test_scores','n_coefs',get_intercept_value])
         
 # Prepare data for plotting  ##################################################
 
-iterator_dict = {'pca':[0.9,0.1],
-                 'regularization type':['l2','l1']}
+# Set the iterator dict (Note, that the order of keys is important here)
+iterator_dict = {'PCA (explained variance)':['90%','10%'],
+                 'Regularization Type':['L2','L1']}
 
+# Skplot is dumb. It doesn't know  which scale each of your extracted measure is using,
+# so you have to provide this manually 
 scale_type_dict = {'lr__C':'nominal',
                    'n_coefs':'interval',
                    'train_score':'interval',
                    'test_score':'interval'}
 
-plot_df = preparation.get_plot_df(scores_df,
+# Use the preparation module to process your data to a plot-ready format
+plot_df = preparation.get_plot_df(plot_data=scores_df,
                                   measures=['test_score','train_score','lr__C'],
                                   measure_labels=['Test Score','Train Score','C'],
+                                  iterator_dict=iterator_dict,
                                   scale_type_dict=scale_type_dict)
 
 # Plot data ###################################################################
 
+# Plot as categorical plots
 g = plotting.catplot_scores(plot_df,
                             iterator_dict=iterator_dict,
                             separate_measures=True,
+                            kind='point',
                             train_scores='train_score')
+# Plot as lineplots
+g = plotting.lineplot_scores(plot_df,
+                             col_wrap=2,
+                             x_tick_spacing=1)
